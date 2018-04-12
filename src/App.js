27c93ab514dfa13cd4e68aa8afdea8e8ac069d60
Michel Engelen <michel@nomorejs.net>
2018-04-12 12:54:41 +0200

    added common components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+
+// import project files
 import reducers from './reducers';
+import { LoadingIndicator, Button } from './components/common';
 
 const App = () => {
   return (
-    <Provider
-      store={createStore(reducers)}
-    >
-      <View />
+    <Provider store={createStore(reducers)}>
+      <View style={{ flex: 1, paddingTop: 15, backgroundColor: '#2a557a' }}>
+        <StatusBar barStyle={'light-content'} />
+        <LoadingIndicator size={'full-size'} />
+        <Button loading={false} buttonText={'Click Here!'} />
+        <Button loading={true} buttonText={'Click Here!'} />
+      </View>
     </Provider>
   );
 };
